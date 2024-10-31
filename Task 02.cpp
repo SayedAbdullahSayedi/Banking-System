881832681ab0ebe280f57907e4219b1da1100ccd
Sayed_Abdullah_Sadaat <sayedabdullahsayedi@gmail.com>
2024-10-31 09:52:56 +0500

    Update Task 02.cpp

--- Task 02.cpp
+++ Task 02.cpp
@@ -5,17 +5,17 @@ int main(){
 
   double InitialBalance, DepositedMoney, Withdrawedmoney;
 
-  Cout << "Please enter your initial balance? " << endl;
-  Cin >> InitialBalance;
+  cout << "Please enter your initial balance? " << endl;
+  cin >> InitialBalance;
 
-  Cout << "How much money do you want to deposit? " << endl;
+  cout << "How much money do you want to deposit? " << endl;
   cin >> DepositedMoney;
 
   InitialBalance += DepositedMoney;
-  Cout << "The amount you already have in bank is " << InitialBalance << " Rs" << endl;
+  cout << "The amount you already have in bank is " << InitialBalance << " Rs" << endl;
 
-  Cout << "Please enter the amount of money you want to withdraw from your account? " << endl;
-  Cin >> Withdrawedmoney;
+  cout << "Please enter the amount of money you want to withdraw from your account? " << endl;
+  cin >> Withdrawedmoney;
 
   InitialBalance -= Withdrawedmoney;
   cout << "Your remaining balance is " << InitialBalance << " Rs" << endl;
